1e39e06aeebb90d824d844216e9c3e4d53cd4e6b
Nazmus Sakib <sakib14april@gmail.com>
2021-11-13 00:00:26 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+        <Route path="/about">
+            {/* <About /> */}
+          </Route>
+          <Route path="/users">
+            {/* <Users /> */}
+          </Route>
+          <Route path="/">
+            {/* <Home /> */}
+          </Route>
+          <Route path="*">
+            <h3>Nothing found</h3>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
